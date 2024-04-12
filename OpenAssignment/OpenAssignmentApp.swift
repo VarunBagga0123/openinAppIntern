bf3004e02db3ed435d5445f0fa0b85e7b138ec84
Varun0153 <varun@totalitycorp.com>
2024-04-12 11:40:22 +0530

    Initial Commit

---
+++ OpenAssignment/OpenAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenAssignmentApp.swift
+//  OpenAssignment
+//
+//  Created by Varun Bagga on 12/04/24.
+//
+
+import SwiftUI
+
+@main
+struct OpenAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
