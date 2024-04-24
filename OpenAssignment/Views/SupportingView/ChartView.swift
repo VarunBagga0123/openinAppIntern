d2462dc8910eb10e00e41b0c059a10ae6855239a
varunbagga19 <varunbagga19@gmail.com>
2024-04-24 10:41:48 +0530

    chart added

---
+++ OpenAssignment/Views/SupportingView/ChartView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  OpenAssignment
+//
+//  Created by Varun Bagga on 23/04/24.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
