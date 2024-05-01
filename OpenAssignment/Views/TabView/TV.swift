fa2b92f77450204e9c936100c6697430ae16ae8c
varunbagga19 <varunbagga19@gmail.com>
2024-05-01 14:28:28 +0530

    TabView Added and improvements

---
+++ OpenAssignment/Views/TabView/TV.swift
@@ -0,0 +1,18 @@
+//
+//  TV.swift
+//  OpenAssignment
+//
+//  Created by Varun Bagga on 30/04/24.
+//
+
+import SwiftUI
+
+struct TV: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TV()
+}
